4f9cfde87399b9d1a35a4ba5dade77f1807afcec
Eunah.xyz <149790250+hww-eunahlee@users.noreply.github.com>
2025-02-03 14:09:24 +0000

    Update TTS_elevenlabs.py

--- TTS_elevenlabs.py
+++ TTS_elevenlabs.py
@@ -1,18 +1,20 @@
-import os, elevenlabs
+import os
+from elevenlabs.client import ElevenLabs
 from pythonosc import udp_client
+from elevenlabs import play, save
 
 
 def get_speech(answer):
-    elevenlabs.set_api_key(os.environ.get("ELEVENLABS_KEY"))
-    audio = elevenlabs.generate(
+    client = ElevenLabs(
+        api_key=os.environ.get("ELEVENLABS_KEY"),
+    )
+    #elevenlabs.set_api_key(os.environ.get("ELEVENLABS_KEY"))
+    audio = client.generate(
         text=answer,
         # need a voice id
         voice="Mvi9cp0TjLS2CVndrxqi",
         model="eleven_multilingual_v2",
     )
-    #elevenlabs.play(audio),
+    #play(audio)
     file_path = os.path.join("voices/", "audio.wav")
-
-    with open(file_path, "wb") as f:
-        f.write(audio)
-    pass
+    save(audio, file_path)
